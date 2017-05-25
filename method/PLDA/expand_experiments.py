837a693f9949f4cfc71a73bf5b60e7bf1a454955
Christopher Gropp <cgropp@user001.palmetto.clemson.edu>
2017-05-25 11:28:21 -0400

    Several minor fixes.

--- method/PLDA/expand_experiments.py
+++ method/PLDA/expand_experiments.py
@@ -47,7 +47,7 @@ for iterationCount in itersList:
       settingsFile.write('PLDA_CHUNKS='+METHOD_PARTITIONS+'\n')
       settingsFile.write('PLDA_ITERS='+str(iterationCount)+'\n')
       settingsFile.write('RAW_DOC_FILE="'+FS_HOME+'/raw_data/'+DATASET_NAME+'/corpus"\n')
-      settingsFile.write('PLDA_CORPUS_DIRECTORY="'+FS_HOME+'/methods/PLDA/'+DATASET_NAME+'"\n')
+      settingsFile.write('PLDA_CORPUS_DIRECTORY="'+FS_HOME+'/methods/PLDA/data/'+DATASET_NAME+'"\n')
       settingsFile.write('EXPERIMENT_DIRECTORY="'+FS_HOME+'/methods/PLDA/experiments/'+str(expDirname)+'/'+str(repName)+'"\n')
       settingsFile.close()
       ## copy setup script - maybe the script that calls this one should do that? technically we shouldn't necessarily know where this got placed
