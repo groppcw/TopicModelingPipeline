5e1f6eb7cbc67505ca06c8600c8428b338cbd44a
Chris Gropp <groppcw@gmail.com>
2017-06-09 14:54:45 -0400

    Saves rep name to settings file.

--- method/PLDA/expand_experiments.py
+++ method/PLDA/expand_experiments.py
@@ -49,6 +49,7 @@ for iterationCount in itersList:
       settingsFile.write('RAW_DOC_FILE="'+FS_HOME+'/raw_data/'+DATASET_NAME+'/corpus"\n')
       settingsFile.write('PLDA_CORPUS_DIRECTORY="'+FS_HOME+'/methods/PLDA/data/'+DATASET_NAME+'"\n')
       settingsFile.write('EXPERIMENT_DIRECTORY="'+FS_HOME+'/methods/PLDA/execution/'+str(expDirname)+'/'+str(repName)+'"\n')
+      settingsFile.write('REP_NAME="'+str(repName)+'"\n')
       settingsFile.close()
       ## copy setup script - maybe the script that calls this one should do that? technically we shouldn't necessarily know where this got placed
       #os.system("cp "+FS_HOME+"/TopicModelingPipeline/method/PLDA/01-setupExperiment.sh .")
