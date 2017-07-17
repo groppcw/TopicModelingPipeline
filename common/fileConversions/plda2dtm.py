79c5cffa697b1a32eb5d63b3780735b5e39aeec9
Chris Gropp <groppcw@gmail.com>
2017-07-17 11:17:27 -0400

    Update plda2dtm.py

--- common/fileConversions/plda2dtm.py
+++ common/fileConversions/plda2dtm.py
@@ -16,14 +16,14 @@ vocabfile.close()
 for doc in docsfile:
   numunique = 0
   thisdoc = ""
-	for enum,element in enumerate(doc.strip().split()):
-		if enum % 2 == 0:
-			word = element
+  for enum,element in enumerate(doc.strip().split()):
+    if enum % 2 == 0:
+      word = element
       wordid = vocab[word]
       numunique = numunique + 1
-		else:
-			copies = element
-			thisdoc = thisdoc + " " + wordid + ":" + copies
+    else:
+      copies = element
+      thisdoc = thisdoc + " " + wordid + ":" + copies
   outfile.write(str(numunique) + thisdoc + "\n")
 docsfile.close()
 outfile.close()
