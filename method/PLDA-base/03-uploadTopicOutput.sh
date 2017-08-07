10fff23ce44290899f973ec0c9ac78e000e8f095
Chris Gropp <groppcw@gmail.com>
2017-08-07 12:56:48 -0400

    Adds a bunch of logging info to S3.

--- method/PLDA-base/03-uploadTopicOutput.sh
+++ method/PLDA-base/03-uploadTopicOutput.sh
@@ -15,8 +15,15 @@ ln -s $SETTINGS_DIR/local_models/full.model.normalized $SETTINGS_DIR/results/ful
 
 # Setup links for the top words files:
 # When top words is changed to be a loop, this code will need to be modified to loop as well.
-ln -s $SETTINGS_DIR/top_words/full-model-top${TOP_X}.txt $SETTINGS_DIR/results/full-model-top${TOP_X}.txt
-ln -s $SETTINGS_DIR/top_words/full-model-top${TOP_X}.raw $SETTINGS_DIR/results/full-model-top${TOP_X}.raw
+#ln -s $SETTINGS_DIR/top_words/full-model-top${TOP_X}.txt $SETTINGS_DIR/results/full-model-top${TOP_X}.txt
+#ln -s $SETTINGS_DIR/top_words/full-model-top${TOP_X}.raw $SETTINGS_DIR/results/full-model-top${TOP_X}.raw
+ln -s $SETTINGS_DIR/top_words/* $SETTINGS_DIR/results/
+
+# Setup links for logging information:
+ln -s $SETTINGS_DIR/settings.py $SETTINGS_DIR/results/settings.py
+ln -s $SETTINGS_DIR/status.txt $SETTINGS_DIR/results/status.txt
+ln -s $SETTINGS_DIR/slurm* $SETTINGS_DIR/results/
+ln -s $SETTINGS_DIR/*.log $SETTINGS_DIR/results/
 
 # Send everything to Amazon!
 #aws s3 cp $SETTINGS_DIR/results/* s3://cutopicmodeling/temporary_landing_zone/
